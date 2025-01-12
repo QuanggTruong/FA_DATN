a26b5f47917d5030d1eef34b37ece357d63b6682
QuanggTruong <89088423+QuanggTruong@users.noreply.github.com>
2025-01-12 22:43:09 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
 
-export default = {
+export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {
